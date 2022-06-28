945c9bd0e294385ea17511d861d72c35bef73921
Joelayo <joetech.ooj@gmail.com>
2022-06-28 15:53:41 -0700

    [FIX] locked_class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-LockedClass
-"""
+'''
+Prevents the user from dynamically
+creating nes instance attributes
+except it is called first_name
+'''
 
 
 class LockedClass:
-    """ No class or object attributes, can't set
-        Except for first_name
-    """
-    def __setattr__(self, attribute, value):
-        if attribute == "first_name":
-            self.__dict__[attribute] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
+    '''
+    the locked class
+    '''
+    __slots__ = ['first_name']
