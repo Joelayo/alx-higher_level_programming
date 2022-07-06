85d47a70c547b36248a7c47a081fddc9abda7428
Joelayo <joetech.ooj@gmail.com>
2022-07-06 15:02:32 -0700

    Project Files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
+""" Module for task 7 """
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-filename = "add_item.json"
-
+FILENAME = "add_item.json"
+args = sys.argv
+args.pop(0)
 try:
-    json_list = load_from_json_file(filename)
-except:
-    json_list = []
-
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+    items = load_from_json_file(FILENAME)
+except FileNotFoundError as e:
+    with open(FILENAME, "a") as f:
+        f.write('[]\n')
+    items = load_from_json_file(FILENAME)
+items.extend(args)
+save_to_json_file(items, FILENAME)
