c17837b925d7a7a90c52ae981306290b23126e5f
Joelayo <joetech.ooj@gmail.com>
2022-09-06 00:34:46 +0100

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const arr1 = [
-	  'C is fun',
-	  'Python is cool',
-	  'JavaScript is amazing'
-];
-
-for (let i = 0; i < arr1.length; i++) {
-	  console.log(arr1[i]);
+const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+	  console.log(s[i]);
 }
