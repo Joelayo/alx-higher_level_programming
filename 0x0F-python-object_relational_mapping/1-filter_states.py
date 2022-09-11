25d82bea06566e6c69f884c9f32df4140bd3d6da
Joelayo <joetech.ooj@gmail.com>
2022-09-11 16:58:21 +0100

    Project Files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ starting with N
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
         db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
